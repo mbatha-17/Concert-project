b9d320e7ccc924999b2c74ea8ff1a7daa7db8c5f
mbatha-17 <jullymbatha17@gmail.com>
2024-05-11 09:46:41 +0300

    Created concert project

---
+++ script.js
@@ -0,0 +1,9 @@
+let Menu = document.querySelector(".Menu");
+
+function openMenu(){
+    document.querySelector('.Menu').style.display = 'block';
+}
+
+function closeMenu(){
+    document.querySelector('.Menu').style.display = 'none';
+}
